de2596010cf7a263fd1c86de1ae59c92cec8cbea
leeyounggue <leeyg@ecgcorp.co.kr>
2023-05-18 14:55:56 +0900

    테스트

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Button from "./Button";
+
 function App() {
   return (
     <div>
-      <h1>Coder Change</h1>
+        <h1>Coder Change</h1>
+
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,6 @@
+function Button( {text} ) {
+    return (
+        <button>Button</button>
+    )
+}
+export default Button;
