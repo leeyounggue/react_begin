cece60bbfd58c989aa11fbc5ea28f99e2ff9840d
leeyounggue <leeyg@ecgcorp.co.kr>
2023-05-18 22:31:43 +0900

    테스트

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import { useState, useEffect } from "react";
 
 function App() {
     const [loading, setLoading] = useState(true);
-    const [coins, setConins] = useState([]);
+    const [coins, setCoins] = useState([]);
     const [myMoney, myMoneySet] = useState(0);
     const [buyAmount, setAmount] = useState(0);
 
+    const [selCoin, setSelCoin] = useState(0);
+
+
     const onChange = (e) => myMoneySet((currnet) => e.target.value);
+    const onChange2 = (e) => setSelCoin( (current) => e.target.value);
     const onClick = () => {
+
+        setAmount( (current) => Math.floor( selCoin / myMoney) );
 
     };
 
@@ -15,7 +21,7 @@ function App() {
         fetch("https://api.coinpaprika.com/v1/tickers")
             .then((response) => response.json())
             .then((json) => {
-                setConins(json);
+                setCoins(json);
                 setLoading(false);
             });
     }, []);
@@ -24,10 +30,11 @@ function App() {
     <div>
         <h1>bts Graph { loading ? "" : `(${coins.length})`} </h1>
         { loading ? <strong>Loading...</strong> :
-            <select>
+            <select onChange={onChange2}>
+                <option value="">선택필요</option>
                 {
                     coins.map( (item, index) => {
-                        return <option key={index}>{item.name} ({item.symbol}) ${item.quotes.USD.price} USD</option>
+                        return <option key={index} value={item.quotes.USD.price}>{item.name} ({item.symbol}) ${item.quotes.USD.price} USD</option>
                     } )
                 }
             </select>
