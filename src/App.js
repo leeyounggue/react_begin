caad798eceab4126843de4b5475fd9e1d341b88c
leeyounggue <leeyg@ecgcorp.co.kr>
2023-05-18 19:03:27 +0900

    테스트

--- src/App.js
+++ src/App.js
@@ -5,13 +5,22 @@ import { useState, useEffect } from "react";
 function App() {
     const [counter, setValue] = useState(0);
     const onClick = () => setValue((prev) => prev + 1);
+    const onChange = (e) => {
+        setKeyword(e.target.value);
+    }
+    const [keyword, setKeyword] = useState("");
     console.log('tt');
     useEffect(() => {
         console.log("tester");
     }, []);
     return (
     <div>
-        <input type="text" placeholder="Search Keyword Here..." />
+        <input
+            onChange={onChange}
+            type="text"
+            placeholder="Search Keyword Here..."
+            value={keyword}
+        />
         <h1 className={styles.title}>Coder Change</h1>
         <span>{counter}</span>
         <Button text={"Continue"} onClicker={onClick} />
