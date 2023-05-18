c4dcf5761c909d3ab3f119f1643f0585b44e625f
leeyounggue <leeyg@ecgcorp.co.kr>
2023-05-18 14:59:30 +0900

    테스트

--- src/Button.js
+++ src/Button.js
@@ -1,6 +1,12 @@
+import PropTypes from "prop-types";
+
 function Button( {text} ) {
     return (
         <button>{text}</button>
     )
+}
+
+Button.prototype = {
+    text: PropTypes.string.isRequired
 }
 export default Button;
