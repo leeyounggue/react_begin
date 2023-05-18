1ca427d12af58018fedb0dcf06c7c03a08b807cd
leeyounggue <leeyg@ecgcorp.co.kr>
2023-05-18 15:25:47 +0900

    테스트

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import classNames from 'classnames';
+
+const cn = classNames.bind(styles);
 
 function App() {
   return (
     <div>
-        <h1 className={styles.title}>Coder Change</h1>
+        <h1 className={cn('title')}>Coder Change</h1>
         <Button text={"Continue"} />
     </div>
   );
