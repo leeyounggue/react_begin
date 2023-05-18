bdaa74306f3e61417f3915039ff3aab895d49e6e
leeyounggue <leeyg@ecgcorp.co.kr>
2023-05-18 21:29:37 +0900

    테스트

--- src/App.js
+++ src/App.js
@@ -1,57 +1,28 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-import { useState, useEffect, memo } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-    const [counter, setValue] = useState(0);
-    const [keyword, setKeyword] = useState("");
-
-    const [showing, setShowing] = useState(false);
-
-    const onClick2 = () => setShowing((current) => !current);
-    const onClick = () => setValue((prev) => prev + 1);
-    const onChange = (e) => {
-        setKeyword(e.target.value);
-    }
+    const [loading, setLoading] = useState(true);
+    const [coins, setConins] = useState([]);
     useEffect(() => {
-        getConsoleLog("키워드 변경");
-    }, [keyword]);
-    useEffect(() => {
-        getConsoleLog("카운터변경");
-    }, [counter]);
-    useEffect(() => {
-        getConsoleLog("전체 변경");
-    }, [keyword, counter]);
+        fetch("https://api.coinpaprika.com/v1/tickers")
+            .then((response) => response.json())
+            .then((json) => {
+                setConins(json);
+                setLoading(false);
+            });
+    }, []);
 
     return (
     <div>
-        <input
-            onChange={onChange}
-            type="text"
-            placeholder="Search Keyword Here..."
-            value={keyword}
-        />
-        <h1 className={styles.title}>Coder Change</h1>
-        <span>{counter}</span>
-        <Button text={"Continue"} onClicker={onClick} />
-
-        { showing ? <Hello /> : null }
-
-        <button onClick={onClick2}>{ showing ? "Hide" : "Show" }</button>
+        <h1>BTC Graph</h1>
+        { loading ? <strong>Loading...</strong> : null }
+        <ul>
+            { coins.map((coin, index) => {
+                <li key={index}>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD</li>
+            }) }
+        </ul>
     </div>
     );
 }
 
-const MemoizedApp = memo(App);
-
-function getConsoleLog(msg) {
-    console.log(msg);
-}
-
-function Hello() {
-    return (
-        <h1>Checked</h1>
-    )
-}
-
-export default MemoizedApp;
+export default App;
