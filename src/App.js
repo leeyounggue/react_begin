268c321137748bdcc0a2aacf59ffe7edbe17b6d4
leeyounggue <leeyg@ecgcorp.co.kr>
2023-05-18 22:45:26 +0900

    테스트

--- src/App.js
+++ src/App.js
@@ -1,48 +1,31 @@
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 
 function App() {
     const [loading, setLoading] = useState(true);
-    const [coins, setCoins] = useState([]);
-    const [myMoney, myMoneySet] = useState(0);
-    const [buyAmount, setAmount] = useState(0);
+    const [movies, setMovies] = useState([]);
 
-    const [selCoin, setSelCoin] = useState(0);
+    const getMovies = async () => {
+        // https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year
+        const json = await (
+            await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")
+        ).json();
 
+        setMovies(json.data.movies);
+        setLoading(false);
+    }
 
-    const onChange = (e) => myMoneySet((currnet) => e.target.value);
-    const onChange2 = (e) => setSelCoin( (current) => e.target.value);
-    const onClick = () => {
-
-        setAmount( (current) => Math.floor( selCoin / myMoney) );
-
-    };
 
     useEffect(() => {
-        fetch("https://api.coinpaprika.com/v1/tickers")
-            .then((response) => response.json())
-            .then((json) => {
-                setCoins(json);
-                setLoading(false);
-            });
+
     }, []);
 
+    console.log(movies);
     return (
-    <div>
-        <h1>bts Graph { loading ? "" : `(${coins.length})`} </h1>
-        { loading ? <strong>Loading...</strong> :
-            <select onChange={onChange2}>
-                <option value="">선택필요</option>
-                {
-                    coins.map( (item, index) => {
-                        return <option key={index} value={item.quotes.USD.price}>{item.name} ({item.symbol}) ${item.quotes.USD.price} USD</option>
-                    } )
-                }
-            </select>
-        }
-        <div>{buyAmount}</div>
-        <input type="text" value={myMoney} onChange={onChange} />
-        <button onClick={onClick}>Calc</button>
-    </div>
+        <div>
+            <h1>Movie List</h1>
+
+            {loading ? <h1>Loading... </h1> : null}
+        </div>
     );
 }
 
