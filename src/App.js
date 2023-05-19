a3c16a33613686318eec4e1bf3dfb892a6d55b66
leeyounggue <leeyg@ecgcorp.co.kr>
2023-05-19 18:08:47 +0900

    테스터

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ function App() {
     return (
         <Router>
             <Routes>
-                <Route path={`process.env.PUBLIC_URL/`} element={<Home />} />
+                <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
                 <Route
-                    path={`process.env.PUBLIC_URL/list`}
+                    path={`${process.env.PUBLIC_URL}/list`}
                     element={<Home />}
                 />
                 <Route
-                    path={`process.env.PUBLIC_URL/movie/:id`}
+                    path={`${process.env.PUBLIC_URL}/movie/:id`}
                     element={<Detail />}
                 />
             </Routes>
