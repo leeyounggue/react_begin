ca0003ad6ade3b6acc577a72b41860ba76eaa118
leeyounggue <leeyg@ecgcorp.co.kr>
2023-05-19 16:39:47 +0900

    테스터

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-    return null;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/list" element={<Home />} />
+                <Route path="/movie/:id" element={<Detail />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 function Detail() {
+    const { id, ...rest } = useParams();
+    console.log(id);
     return <h1>Detail Page</h1>;
 }
 
