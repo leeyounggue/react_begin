6a4f617caded6f5eaf028739d9782e7a98675e08
leeyounggue <leeyg@ecgcorp.co.kr>
2023-05-19 16:44:50 +0900

    테스터

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -4,8 +4,15 @@ import { useParams } from "react-router-dom";
 function Detail() {
     const { id, ...rest } = useParams();
     const detailAPiUrl = `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`;
+    const [movieDetail, setDetail] = useState([]);
+    const getDetaiiData = async () => {
+        const json = await (await fetch(detailAPiUrl)).json();
+        setDetail(json);
+    };
 
-    console.log(detailAPiUrl);
+    useEffect(() => {
+        getDetaiiData;
+    }, []);
 
     return <h1>Detail Page</h1>;
 }
