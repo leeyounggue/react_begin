5d9987b140c4420e1f7f3a89b7cafdd7d81a095c
leeyounggue <leeyg@ecgcorp.co.kr>
2023-05-19 17:13:40 +0900

    테스터

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
