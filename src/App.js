4851d956c8bf7e1f8733f651105a7a624994c10b
leeyounggue <leeyg@ecgcorp.co.kr>
2023-05-19 15:06:15 +0900

    테스터

--- src/App.js
+++ src/App.js
@@ -1,45 +1,7 @@
 import React, { useState, useEffect } from "react";
-import Movie from "./components/Movie.js";
 
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-
-    const getMovies = async () => {
-        const json = await (
-            await fetch(
-                "https://yts.mx/api/v2/list_movies.json?minimum_rating=9.5&sort_by=year"
-            )
-        ).json();
-
-        setMovies(json.data.movies);
-        setLoading(false);
-    };
-
-    useEffect(() => {
-        getMovies();
-    }, []);
-
-    return (
-        <div>
-            <h1>Movie List</h1>
-            {loading ? (
-                <h1>Loading...</h1>
-            ) : (
-                <div>
-                    {movies.map((movie) => (
-                        <Movie
-                            key={movie.id}
-                            coverImg={movie.medium_cover_image}
-                            title={movie.title}
-                            summary={movie.summary}
-                            genres={movie.genres}
-                        />
-                    ))}
-                </div>
-            )}
-        </div>
-    );
+    return null;
 }
 
 export default App;
---
+++ src/routes/Detail.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Detail() {
+    return <h1>Detail Page</h1>;
+}
+
+export default Detail;
